eb624e6b14327720e9d592c3927fc900f510a945
Teja Rekapalli <rekapalliteja1@gmail.com>
2026-08-11 12:49:58 +0530

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Experience from "./components/Experience";
-import Expertise from "./components/Expertise";
-import Resume from "./components/Resume";
-
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import About from "./components/about";
+import Skills from "./components/skills";
+import Projects from "./components/projects";
+import Contact from "./components/contact";
+import Footer from "./components/footer";
+import Experience from "./components/experience";
+import Expertise from "./components/expertise";
+import Resume from "./components/resume";
 
 function App() {
   return (
     <div className="bg-[#0f172a] text-white">
       <Navbar />
+
       <Hero />
+
       <About />
-      <Expertise/>
+
+      <Expertise />
+
       <Skills />
-        <Experience />
+
+      <Experience />
+
       <Projects />
+
       <Resume />
+
       <Contact />
+
       <Footer />
     </div>
   );
